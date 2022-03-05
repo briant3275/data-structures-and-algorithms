b4da7e627da046272f8d5c7af29b7e8663d80ce4
Brian Thornburg <briant3275@gmail.com>
2022-03-04 23:24:03 -0600

    fixed code challenge 12

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -8,7 +8,7 @@ Write a function named replaceZeros that, given a string, uses Regex to replace 
 
 const replaceZeros = (string) => {
   // Solution code here...
-  let regex = /[0]/gm;
+  let regex = /[0]/g;
   return string.replace(regex, 'zero');
 };
 
@@ -22,13 +22,9 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 
 const validatePin = (pin) => {
   // Solution code here...
-  let regex = /[0-9]{4}/gm;
-  if (pin.toString.length === regex) {
-    return true;
-  }
-  else {
-    return false;
-  }
+  let regex = /^\d{4}$/g;
+
+  return regex.test(pin);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -41,13 +37,10 @@ If the word is between 5 and 10 characters long, return true. Otherwise, return 
 
 const validateWord = (word) => {
   // Solution code here...
-  let regex = /[A-Za-z]{5,10}/gm;
-  if (word === regex) {
-    return true;
-  }
-  else {
-    return false;
-  }
+  let regex = /^[A-Za-z]{5,10}$/g;
+
+  return regex.test(word);
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -60,6 +53,9 @@ If it does, return true. If not, return false.
 
 const hasNumber = (string) => {
   // Solution code here...
+  let regex = /[A-Za-z]+\d+/g;
+
+  return regex.test(string);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -80,6 +76,9 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 
 const validateEmail = (email) => {
   // Solution code here...
+  let regex = /^[A-Za-z0-9]+\.?[A-za-z0-9]+?@[A-Za-z0-9]+.(net|com|org)$/gm;
+
+  return regex.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -105,6 +104,9 @@ Return either true or false.
 
 const validatePhoneNumber = (phoneNumber) => {
   // Solution code here...
+  let regex = /^(\(\d{3}\)|\d{3})[ -]?\d{3}[ -]?\d{4}$/gm;
+
+  return regex.test(phoneNumber);
 };
 
 /* ------------------------------------------------------------------------------------------------
